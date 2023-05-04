b7aded0a7b4719ce332291b62b5f80e1c0307d4d
akakik <83101560+akakik@users.noreply.github.com>
2023-05-04 21:45:57 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,257 @@
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+      
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var imga = document.getElementById("myImg1");
+  var imgb = document.getElementById("myImg2");
+  var imgc = document.getElementById("myImg3");
+  var imgd = document.getElementById("myImg4");
+  var imge = document.getElementById("myImg5");
+  var imgf = document.getElementById("myImg6");
+  var imgg = document.getElementById("myImg7");
+  var imgh = document.getElementById("myImg8");
+  var imgi = document.getElementById("myImg9");
+  var imgj  = document.getElementById("myImg10");
+  var imgk = document.getElementById("myImg11");
+  var imgl = document.getElementById("myImg12");
+  var imgm = document.getElementById("myImg13");
+  var imgn = document.getElementById("myImg14");
+  var imgo = document.getElementById("myImg15");
+  var imgp = document.getElementById("myImg16");
+  var imgq = document.getElementById("myImg17");
+  var imgr = document.getElementById("myImg18");
+  var imgs = document.getElementById("myImg19");
+  var imgt = document.getElementById("myImg20");
+  var imgu = document.getElementById("myImg21");
+  var imgv = document.getElementById("myImg22");
+  var imgw = document.getElementById("myImg23");
+  var imgx = document.getElementById("myImg24");
+  var imgy = document.getElementById("myImg25");
+  var imgz = document.getElementById("myImg26");
+  var imgaa = document.getElementById("myImg27");
+  var imghb= document.getElementById("myImg28");
+  var imgic= document.getElementById("myImg29");
+  var imgjd = document.getElementById("myImg30");
+  var imgke= document.getElementById("myImg31");
+  var imglf= document.getElementById("myImg32");
+  var imgmg= document.getElementById("myImg33");
+  var imgnh= document.getElementById("myImg34");
+  var imgoi= document.getElementById("myImg35");
+  var imgpj= document.getElementById("myImg36");
+  var imgqk= document.getElementById("myImg37");
+  var imgrl= document.getElementById("myImg38");
+  var imgsm= document.getElementById("myImg39");
+  var imgtn= document.getElementById("myImg40");
+
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  imga.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgb.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgc.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgd.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imge.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgf.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgg.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgh.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgi.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgj.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgk.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgl.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgm.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgn.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgo.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgp.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgq.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgr.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgs.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgt.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgu.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgv.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgw.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgx.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgy.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgz.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgaa.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imghb.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgic.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgjd.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgke.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imglf.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgmg.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgnh.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgoi.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgpj.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgqk.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgrl.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgsm.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  imgtn.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+ 
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
